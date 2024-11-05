51345ddaeb471f5ea85a3b45a61f5d040a3414da
Yohan <ztf1205@naver.com>
2024-11-05 15:36:22 +0900

    메인1 코드 추가

---
+++ git pracice/Assets/main1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //34313r1r
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
